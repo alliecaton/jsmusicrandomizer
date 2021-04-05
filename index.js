19b856c4352a711566201439c8fc29126e01ed9f
Allie <alliecaton3@gmail.com>
2021-04-05 13:02:18 -0400

    moved lastfm api stuff into a class

--- index.js
+++ index.js
@@ -1,57 +1,46 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const searchForm = document.getElementById("song-search")
+    searchForm.addEventListener("submit", (e)=> {
+        e.preventDefault()
+        const ul = document.getElementById('song-list')
 
-// function songFetcher(name) {
-//     return fetch('https://ws.audioscrobbler.com/2.0/?method=track.search&track=Believe&api_key=efeaa32576655308d8b417be9812fc15&format=json') 
-//     .then(function(response) {
-//     return response.json();
-//     })
-//     .then(function(json) {
-//         console.log(json.results.trackmatches.track[0].name, json.results.trackmatches.track[0].artist)
-//     }); 
-// }
-
-
-function getRelated(name, artist) {
-    return fetch(`https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${artist.toLowerCase().trim()}&track=${name.toLowerCase().trim()}&api_key=efeaa32576655308d8b417be9812fc15&format=json`) 
-    .then(function(response) {
-        return response.json();
+        const songInput = document.getElementById('song-input-field').value
+        const artistInput = document.getElementById('artist-input-field').value
+        apiCall = new lastFmApi(songInput, artistInput)
+        apiCall.getRelated()
     })
-    .then(function(json) {
-        console.log(json)
-        let randomNum = Math.floor((Math.random() * 100))
-        let songObject = json.similartracks.track[`${randomNum}`]
-        displayResults(songObject)
-    })
-}
-
-const searchForm = document.getElementById("song-search")
-searchForm.addEventListener("submit", (e)=> {
-    e.preventDefault()
-    const ul = document.getElementById('song-list')
-
-    const songInput = document.getElementById('song-input-field')
-    const artistInput = document.getElementById('artist-input-field')
-    getRelated(songInput.value, artistInput.value)
 })
 
-
-function displayResults(object) {
-    const ul = document.getElementById('song-list')
-    const link = document.createElement('a')
-    while (ul.firstChild) {
-        ul.firstChild.remove()
+class lastFmApi {
+    constructor(name, artist) {
+        this.name = name
+        this.artist = artist
     }
-    li = document.createElement('li')
-    li.innerText = `${object.name} - ${object.artist.name}`
-    link.innerText = "Play Song"
-    link.setAttribute('href', object.url)
-    li.appendChild(link)
-    ul.appendChild(li)
-}
+    
+    getRelated() {
+        return fetch(`https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${this.artist.toLowerCase().trim()}&track=${this.name.toLowerCase().trim()}&api_key=efeaa32576655308d8b417be9812fc15&format=json`) 
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            console.log(json)
+            let randomNum = Math.floor((Math.random() * 100))
+            let songObject = json.similartracks.track[`${randomNum}`]
+            lastFmApi.displayResults(songObject)
+        })
+    }
 
-
-class Song {
-
-
-
-
+    static displayResults(object) {
+        const ul = document.getElementById('song-list')
+        const link = document.createElement('a')
+        while (ul.firstChild) {
+            ul.firstChild.remove()
+        }
+        const li = document.createElement('li')
+        li.innerText = `${object.name} - ${object.artist.name}`
+        link.innerText = "Play Song"
+        link.setAttribute('href', object.url)
+        li.appendChild(link)
+        ul.appendChild(li)
+    }
 }
